6fabac74cdc959e0dd3c4bb98c0f07cb61fd00d2
lucioineichen <78795594+lucioineichen@users.noreply.github.com>
2024-09-11 17:36:49 +0200

    use file

--- src/app/shared/ui/task/task.module.ts
+++ src/app/shared/ui/task/task.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MaterialModule } from '../material.module'
 import { TaskComponent } from './task.component'
+import { FileModule } from '../file/file.module'
 
 @NgModule({
   declarations: [TaskComponent],
   exports: [TaskComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, FileModule],
 })
 export class TaskModule {}
